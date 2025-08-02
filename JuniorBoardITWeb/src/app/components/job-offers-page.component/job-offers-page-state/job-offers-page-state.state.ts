e1c46e523792e177cc6a758aa3fdd2d0f09ad709
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-08-02 14:16:21 +0200

    1.36 - JobOffers & Reports models

--- JuniorBoardITWeb/src/app/components/job-offers-page.component/job-offers-page-state/job-offers-page-state.state.ts
+++ JuniorBoardITWeb/src/app/components/job-offers-page.component/job-offers-page-state/job-offers-page-state.state.ts
@@ -1,43 +1,11 @@
-import { CategoryEnum } from 'src/app/enums/JobOffers/CategoryEnum';
-import { CurrencyEnum } from 'src/app/enums/JobOffers/CurrencyEnum';
-import { EducationEnum } from 'src/app/enums/JobOffers/EducationEnum';
-import { EmploymentTypeEnum } from 'src/app/enums/JobOffers/EmploymentTypeEnum';
-import { ExpirenceEnum } from 'src/app/enums/JobOffers/ExpirenceEnum';
-import { LocationEnum } from 'src/app/enums/JobOffers/LocationEnum';
-import { SalaryEnum } from 'src/app/enums/JobOffers/SalaryEnum';
-import { StatusEnum } from 'src/app/enums/JobOffers/StatusEnum';
+import { Filters, JobOffer } from '../job-offers-page.models';
 
 export const featureKeyJobOffersState = 'job-offers-page-state';
 
 export interface JobOffersState {
-  JobOffers: any[];
-  JobOffer: {
-    JOTitle: string;
-    JOCompanyName: string;
-    JOLocationType: LocationEnum;
-    JOOfficeLocation: string;
-    JOEmploymentType: EmploymentTypeEnum;
-    JOExpirenceLevel: ExpirenceEnum;
-    JOExpirenceYears: number;
-    JOCategory: CategoryEnum;
-    JOSalaryMin: number;
-    JOSalaryMax: number;
-    JOCurrency: CurrencyEnum;
-    JOSalaryType: SalaryEnum;
-    JODescription: string;
-    JORequirements: string;
-    JOBenefits: string;
-    JOEducation: EducationEnum;
-    JOCreatedAt: Date;
-    JOPostedAt: Date;
-    JOExpiresAt: Date;
-    JOStatus: StatusEnum;
-  };
-  Filters: {
-    Skip: number;
-    Take: number;
-    Education: EducationEnum;
-  };
+  JobOffers: JobOffer[];
+  JobOffer: JobOffer;
+  Filters: Filters;
   JobOffersCount: number;
   ErrorMessage: string;
 }
--- JuniorBoardITWeb/src/app/components/reports-page.component/reports-page-state/reports-page-state.state.ts
+++ JuniorBoardITWeb/src/app/components/reports-page.component/reports-page-state/reports-page-state.state.ts
@@ -1,52 +1,13 @@
-import { CategoryEnum } from 'src/app/enums/JobOffers/CategoryEnum';
-import { CurrencyEnum } from 'src/app/enums/JobOffers/CurrencyEnum';
-import { EmploymentTypeEnum } from 'src/app/enums/JobOffers/EmploymentTypeEnum';
-import { ExpirenceEnum } from 'src/app/enums/JobOffers/ExpirenceEnum';
-import { LocationEnum } from 'src/app/enums/JobOffers/LocationEnum';
-import { SalaryEnum } from 'src/app/enums/JobOffers/SalaryEnum';
-import { StatusEnum } from 'src/app/enums/JobOffers/StatusEnum';
-import { ReportsStatusEnum } from 'src/app/enums/Reports/ReportsStatusEnum';
+import { JobOffer } from '../../job-offers-page.component/job-offers-page.models';
+import { Filters, Report, Reports } from '../reports-page.models';
 
 export const featureKeyReportsState = 'reports-page-state';
 
 export interface ReportsState {
-  Reports: any[];
-  Report: {
-    RID: number;
-    RGID: string;
-    RJOGID: string;
-    RReporterGID: string;
-    RSupportGID: string;
-    RDate: Date;
-    RReasons: string;
-    RText: string;
-    RStatus: ReportsStatusEnum;
-  };
-  JobOffer: {
-    JOTitle: string;
-    JOCompanyName: string;
-    JOLocationType: LocationEnum;
-    JOOfficeLocation: string;
-    JOEmploymentType: EmploymentTypeEnum;
-    JOExpirenceLevel: ExpirenceEnum;
-    JOExpirenceYears: number;
-    JOCategory: CategoryEnum;
-    JOSalaryMin: number;
-    JOSalaryMax: number;
-    JOCurrency: CurrencyEnum;
-    JOSalaryType: SalaryEnum;
-    JODescription: string;
-    JORequirements: string;
-    JOBenefits: string;
-    JOCreatedAt: Date;
-    JOPostedAt: Date;
-    JOExpiresAt: Date;
-    JOStatus: StatusEnum;
-  };
-  Filters: {
-    Skip: number;
-    Take: number;
-  };
+  Reports: Reports[];
+  Report: Report;
+  JobOffer: JobOffer;
+  Filters: Filters;
   ReportsCount: number;
   ErrorMessage: string;
 }
